e7cd7cb0f5c31d09e2525b1d2b0f925f9e9bbf83
Matthew Duff <matthewlduff@gmail.com>
2013-09-11 08:22:25 -0600

    removed everything, first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeApi::Application.config.secret_key_base = 'd1522a947471b19d78b28d7286b131b63688bc658f0c7f9a1a78bc236d617df6edc917e4c061b7cfba16a08eb38f3581497c473114d7563768ae8a6979c8ed00'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimeApi::Application.config.session_store :cookie_store, key: '_time_api_session'
