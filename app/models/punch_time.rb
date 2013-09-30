b695ca6fe61bf7e89532a4624ce6b1dd77633f3b
Matt Duff <matthewlduff@gmail.com>
2013-09-29 20:11:19 -0600

    added the before_create validation

--- app/models/punch_time.rb
+++ app/models/punch_time.rb
@@ -5,5 +5,22 @@ class PunchTime < ActiveRecord::Base
   belongs_to :organization
 
   validates_presence_of :in
+  before_create :ensure_punches_complete
+
+  def ensure_punches_complete
+    total = @user.punch_times.length
+    puts "How Many?"
+    puts total
+    puts ""
+    new_total = total.take_while { |punch_time| punch_time.in && punch_time.out != nil }
+    puts "How Many Now?"
+    puts new_total
+    puts ""
+    if total == new_total 
+      return true
+    else
+      return false
+    end
+  end
 
 end
