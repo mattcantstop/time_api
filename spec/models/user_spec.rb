58ccb1ace0719abaa5d29d82fa5b40d2d6347152
Matt Duff <matthewlduff@gmail.com>
2013-10-01 17:41:39 -0600

    finished custom method specs for punch_times too

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+# STRONGLY NEED TO DRY THIS UP!
 describe User do
   it "should have an email" do
     user = User.new
@@ -11,6 +12,22 @@ describe User do
     punch2 = PunchTime.new(:in => "#{Time.now}")
     user = User.new(:punch_times => [punch1, punch2])
     user.punch_times.should include(punch1)
+  end
+
+  describe "#all_punches_complete?" do
+    context "when all punches have both in and out" do
+      punch1 = PunchTime.new(:in => DateTime.now, :out => DateTime.now)
+      punch2 = PunchTime.new(:in => DateTime.now, :out => DateTime.now)
+      let(:user) { User.new(email: "matthewlduff@gmail.com", punch_times: [punch1, punch2]) }
+      specify { user.all_punches_complete?.should be_true }
+    end
+
+    context "when a punch has in but not out" do
+      punch1 = PunchTime.new(:in => DateTime.now)
+      let(:user) { User.new(email: "matthewlduff@gmail.com", punch_times: [punch1]) }
+      specify { user.all_punches_complete?.should be_false }
+    end
+
   end
 
 end
