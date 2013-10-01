ffdf0ac41e68ea8563d5889c212a856ee4f6ce4a
Matthew Duff <matthewlduff@gmail.com>
2013-09-30 23:59:06 -0600

    a remedial, mildly useful test while I learn a little more

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
 
-describe "User" do
-  it "has a name"
-  
+describe User do
+  it "should have an email" do
+    user = User.new
+    user.should_not be_valid
+  end 
+
+  it "should not be valid without an in time" do
+    punch1 = PunchTime.new(:in => "#{Time.now}")
+    punch2 = PunchTime.new(:in => "#{Time.now}")
+    user = User.new(:punch_times => [punch1, punch2])
+    user.punch_times.should include(punch1)
+  end
 
 end
