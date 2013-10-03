8f8c5edb8e58e6231459676c75af90266f8aa581
Matt Duff <matthewlduff@gmail.com>
2013-10-02 20:03:33 -0600

    removed value if it is nil

--- lib/time_parse.rb
+++ lib/time_parse.rb
@@ -1,6 +1,5 @@
 module Duration
 
-
   def self.calculate(duration)
     times = {years: 31536000, months: 2678400, weeks: 604800, days: 86400, hours: 3600, minutes: 60, seconds: 1}
     response = ''
@@ -8,14 +7,14 @@ module Duration
     times.each do |time, value|
       numerator = duration / value if duration >= value
       duration = duration % value if numerator
-      response << "#{numerator} #{time} "
+      response << "#{numerator} #{time} " if numerator
     end
     puts response
   end
 
 end
 
-Duration.calculate(99030720045)
+Duration.calculate(99720045)
 
 
 
