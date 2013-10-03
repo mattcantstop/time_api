486e156c46b08b44fabdc441ed4536b62b6940cf
Matt Duff <matthewlduff@gmail.com>
2013-10-02 20:07:26 -0600

    removed months, it was jacking up what things meant

--- lib/time_parse.rb
+++ lib/time_parse.rb
@@ -1,7 +1,7 @@
 module Duration
 
   def self.calculate(duration)
-    times = {years: 31536000, months: 2678400, weeks: 604800, days: 86400, hours: 3600, minutes: 60, seconds: 1}
+    times = {years: 31536000, months: 2678400, days: 86400, hours: 3600, minutes: 60, seconds: 1}
     response = ''
     duration = duration
     times.each do |time, value|
