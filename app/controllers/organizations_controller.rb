ddde2fe12d13b1103c340fede3f46107178793b4
Matt Duff <matthewlduff@gmail.com>
2013-10-08 19:25:52 -0600

    adding form for new organization

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,14 +1,19 @@
 class OrganizationsController < ApplicationController
 
+  before_filter :get_user
+  
   def new
-    @organization = @user.organization.new(organization_params)
+    @organization = @user.organizations.new(organization_params)
   end
 
   private
 
   def organization_params
-    params.require(:organization).require(:name, :user_id)
+    params.require(:organization).permit(:name, :user_id)
   end
 
+  def get_user
+    @user = current_user
+  end
 
 end
