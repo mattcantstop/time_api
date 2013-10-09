40c7bd12594c14a3ecbc13216fd484dc8d7e53af
Matt Duff <matthewlduff@gmail.com>
2013-10-08 19:12:19 -0600

    migrated and added some organization controller methods

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,2 +1,14 @@
 class OrganizationsController < ApplicationController
+
+  def new
+    @organization = @user.organization.new(organization_params)
+  end
+
+  private
+
+  def organization_params
+    params.require(:organization).require(:name, :user_id)
+  end
+
+
 end
