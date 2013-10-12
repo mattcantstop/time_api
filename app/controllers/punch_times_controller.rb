0340bd489460cf62c16f662415944652019a355b
Matthew Duff <matthewlduff@gmail.com>
2013-10-12 09:21:25 -0600

    org is now included in the punch

--- app/controllers/punch_times_controller.rb
+++ app/controllers/punch_times_controller.rb
@@ -18,6 +18,7 @@ class PunchTimesController < ApplicationController
     @punch_time = @user.punch_times.new(punch_time_params)
     @punch_time.in = Time.now
     @punch_time.project_id = params[:project][:project_id]
+    @punch_time.organization_id = params[:organization][:organization_id]
     if @punch_time.save
       redirect_to user_punch_times_path(@user)
       flash[:notice] = "Successfully clocked in."
