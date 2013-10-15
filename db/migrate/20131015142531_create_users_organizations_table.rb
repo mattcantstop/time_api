159d4f93358a52a1b8ffc68eed47f67849f074fa
Matt Duff <matthewlduff@gmail.com>
2013-10-15 08:31:23 -0600

    added migration for user/org join table

---
+++ db/migrate/20131015142531_create_users_organizations_table.rb
@@ -0,0 +1,8 @@
+class CreateUsersOrganizationsTable < ActiveRecord::Migration
+  def change
+    create_table :users_organizations do |t|
+      t.integer :user_id
+      t.integer :organization_id
+    end
+  end
+end
